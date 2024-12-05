c205da11f1b442aeade0b58709c3ff0219c9c21d
CypherHarsh <167523521+CypherHarsh@users.noreply.github.com>
2024-12-05 23:40:22 +0530

    Update server.py

--- server.py
+++ server.py
@@ -25,5 +25,4 @@ def authenticate():
     return jsonify({"success": True, "message": "Authentication successful"}), 200
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
