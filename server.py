7fcd71020954f6aa38b35ccd6aaa83eba7dd6fca
CypherHarsh <167523521+CypherHarsh@users.noreply.github.com>
2024-12-27 09:25:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 # In-memory user database (for example purposes)
 user_data = {
-    "shivam2312.be22@chitkara.edu.in": {"authenticated": False},
-    "ritika2168.be22@chitkara.edu.in": {"authenticated": False},
-    "nandini2586.be22@chitkara.edu.in": {"authenticated": False},
+    # "shivam2312.be22@chitkara.edu.in": {"authenticated": False},
+    # "ritika2168.be22@chitkara.edu.in": {"authenticated": False},
+    # "nandini2586.be22@chitkara.edu.in": {"authenticated": False},
 }
 
 @app.route('/authenticate', methods=['POST'])
