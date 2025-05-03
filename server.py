dc07f2bfb2fc64bf6cab61d3697fd2134381b975
CypherHarsh <167523521+CypherHarsh@users.noreply.github.com>
2025-05-03 12:25:11 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,8 +7,13 @@ user_data = {
     "arshdeep1366@chitkara.edu.in": {"authenticated": False},
     "ashish1401.be22@chitkara.edu.in": {"authenticated": False},
     "naman594.be22@chitkara.edu.in": {"authenticated": False},
-    "prikshit2087.be22@chitkara.edu.in": {"authenticated": False},
+    "paras637.be22@chitkara.edu.in": {"authenticated": False},
     "manish1881.be22@chitkara.edu.in": {"authenticated": False},
+    "gaurav1570.be22@chitkara.edu.in": {"authenticated": False},
+    "gauri1574.be22@chitkara.edu.in": {"authenticated": False},
+    "rakshit1406.be23@chitkara.edu.in": {"authenticated": False},
+    "tanisha2226.be23@chitkara.edu.in": {"authenticated": False},
+    "kartik1751.be22@chitkara.edu.in": {"authenticated": False},
     
     
     }
